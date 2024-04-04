7b0e7711cd2e20e71710816531f8cc1600824a90
YunheeAhn <ayh95944674@gmail.com>
2024-04-05 02:50:24 +0900

    [적용]글로벌 스타일 상단 루트 적용

--- pot_accounts/src/main/pot_front/src/index.js
+++ pot_accounts/src/main/pot_front/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* 글로벌 스타일 적용 컴포넌트 */}
+    <GlobalStyle/>
   </React.StrictMode>
 );
 
