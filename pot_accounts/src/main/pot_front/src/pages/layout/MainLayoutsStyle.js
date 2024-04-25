c018122782f891fb9ed8ba926132c2c43239a568
YunheeAhn <ayh95944674@gmail.com>
2024-04-25 20:31:06 +0900

    메인레이아웃 스타일 추가

---
+++ pot_accounts/src/main/pot_front/src/pages/layout/MainLayoutsStyle.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Full = styled.div`
+display : flex;
+flex-direction : row;
+width : 100%;
+`
+
+export const Inner = styled.div`
+width: calc(100% - 300px);
+padding : 60px;
+`
+export const Container = styled.div`
+width : 100%;
+display : flex;
+gap : 7px;
+margin-bottom : 35px;
+`
+export const MainTitle = styled.h1`
+font-size : 52px;
+font-weight : 700;
+
+color : #20690B;
+`
+
+export const MainSpan = styled.p`
+font-size : 52px;
+font-weight : 700;
+`
