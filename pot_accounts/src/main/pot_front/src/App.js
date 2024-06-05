5d613fd9a20c1cdc43242e1b8535636dfdde7a56
YunheeAhn <ayh95944674@gmail.com>
2024-06-05 18:59:23 +0900

    불필요 임포트 제거

--- pot_accounts/src/main/pot_front/src/App.js
+++ pot_accounts/src/main/pot_front/src/App.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import SignUp from './pages/signup/Signup';
 import Routers from './Routers';
-import Login from './pages/login/Login';
-import Header from './pages/layout/Header';
-import { Outlet } from 'react-router-dom';
 
 function App() {
     const [elist, setElist] = useState([]);
