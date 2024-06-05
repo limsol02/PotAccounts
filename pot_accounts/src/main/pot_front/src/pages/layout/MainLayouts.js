decdbe8667a9c4dbe000a3943c56fad9c1ce1729
YunheeAhn <ayh95944674@gmail.com>
2024-06-05 19:39:55 +0900

    레이아웃-메인 설정,수정

--- pot_accounts/src/main/pot_front/src/pages/layout/MainLayouts.js
+++ pot_accounts/src/main/pot_front/src/pages/layout/MainLayouts.js
@@ -1,8 +1,37 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header"
-import { Container, Full, Inner, MainSpan, MainTitle } from "./MainLayoutsStyle";
+import { AddBookButton, ButtonContainer, Container, CreateBookButton, Full, Inner, MainSpan, MainTitle } from "./MainLayoutsStyle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Mainlayout = () => {
+
+    // 버튼 클릭시 상태 변화
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    // 가계부 생성 상태 변화
+    const [newBookName, setNewBookName] = useState('');
+    const [bookList, setBookList] = useState([]);
+
+    // 버튼 클릭 이벤트(아이콘 색상변환)
+    const handleAddBookClick = () => {
+        setSelectedButton('addBook');
+    };
+    
+    // 공유가계부 생성 클릭 이벤트
+    const handleCreateBookClick = () => {
+        setSelectedButton('createBook');
+
+        // 새로운 가계부 생성
+        if (newBookName) {
+            // 새로운 가계부 이름을 리스트에 추가
+            setBookList([...bookList, newBookName]);
+            // 입력 필드 초기화
+            setNewBookName('');
+        }
+    };
+
     return (
         <>
             <Full>
@@ -12,7 +41,30 @@ const Mainlayout = () => {
                         <MainTitle>안윤희</MainTitle>
                         <MainSpan>님</MainSpan>
                     </Container>
-                    
+
+                    <ButtonContainer>
+                        <AddBookButton
+                            onClick={handleAddBookClick}
+                        >
+                            <FontAwesomeIcon className={'selected'} icon={faCircle} />
+                            <p>안윤희님의 가계부</p>
+                        </AddBookButton>
+
+                        {/* 아이콘이 faPlus인 버튼 누른 후 가계부 추가 하면 생기는 버튼 */}
+                        {bookList.map((bookName, index) => (
+                            <AddBookButton key={index}>
+                                <FontAwesomeIcon className={selectedButton === 'addBook' ? 'selected' : ''} icon={faCircle} />
+                                <p>{bookName}</p>
+                            </AddBookButton>
+                        ))}
+
+                        <CreateBookButton
+                            onClick={handleCreateBookClick}
+                        >
+                            <FontAwesomeIcon className={selectedButton === 'createBook' ? 'selected' : ''} icon={faPlus} />
+                        </CreateBookButton>
+                    </ButtonContainer>
+
                     <Outlet/>
                 </Inner>
             </Full>
