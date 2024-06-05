1a7159addd098f93291139b71743214d555b9d72
YunheeAhn <ayh95944674@gmail.com>
2024-06-05 19:40:13 +0900

    레이아웃-메인 스타일 컴포넌트 설정

--- pot_accounts/src/main/pot_front/src/pages/layout/MainLayoutsStyle.js
+++ pot_accounts/src/main/pot_front/src/pages/layout/MainLayoutsStyle.js
@@ -4,6 +4,7 @@ export const Full = styled.div`
 display : flex;
 flex-direction : row;
 width : 100%;
+height : 100%;
 `
 
 export const Inner = styled.div`
@@ -26,4 +27,49 @@ color : #20690B;
 export const MainSpan = styled.p`
 font-size : 52px;
 font-weight : 700;
+`
+
+export const AddBookButton = styled.button`
+border-radius: 20px;
+border: 1px #c7c7c7 solid;
+background: #ffffff;
+font-size : 20px;
+
+height: 42px;
+padding : 15px;
+
+display : flex;
+align-items: center;
+gap : 10px;
+
+&:hover {
+    background : #fff;
+    color : #222;
+    border : 1px solid #20690B;
+}
+`
+export const CreateBookButton = styled.button`
+border-radius: 20px;
+border: 1px #c7c7c7 solid;
+background: #ffffff;
+font-size : 20px;
+
+height: 42px;
+padding : 15px;
+
+display : flex;
+align-items: center;
+gap : 10px;
+
+&:hover {
+    background : #fff;
+    color : #222;
+    border : 1px solid #20690B;
+}
+`
+export const ButtonContainer = styled.div`
+width : 100%;
+display : flex;
+gap : 20px;
+margin-bottom : 35px;
 `
