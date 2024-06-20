b5ce2d03f38dedbf3d0a53594e9f3544a646784e
limsol02 <yesshol@naver.com>
2024-06-20 23:57:48 +0900

    메인 주별분석 수입/지출 해당하는 달의 주차별 분석 약간수정

--- pot_accounts/src/main/pot_front/src/api/accounts.js
+++ pot_accounts/src/main/pot_front/src/api/accounts.js
@@ -2,17 +2,15 @@ import axios from "axios";
 import API from "../config/apiConfig";
 
 // 기본설정
-const apiFun = axios.create({
+const authorizationClient = axios.create({
     baseURL: API.BASE_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
 });
-
-export default apiFun;
 // get 요청
-apiFun.get('/some-endpoint')
+authorizationClient.get('/some-endpoint')
     .then(response => {
         console.log('데이터:', response.data);
     })
@@ -21,7 +19,7 @@ apiFun.get('/some-endpoint')
     });
 
 // post 요청
-apiFun.post('/another-endpoint', { key: 'value' })
+authorizationClient.post('/another-endpoint', { key: 'value' })
     .then(response => {
         console.log('응답:', response.data);
     })
@@ -34,7 +32,7 @@ apiFun.post('/another-endpoint', { key: 'value' })
 
 // 카테고리데이터 불러오기
 export const loadCategory = async (id) => {
-    const { data } = await apiFun.get(
+    const { data } = await authorizationClient.get(
         `${API.ACCOUNT}/${API.USER}/${id}/${API.CATEGORY}`,
     );
     return data;
@@ -43,7 +41,7 @@ export const loadCategory = async (id) => {
 // 수입내역 불러오기
 export const loadIncome = async (historyData) => {
     const { id, page, size } = historyData;
-    const { data } = await apiFun.get(
+    const { data } = await authorizationClient.get(
         `${API.ACCOUNT}/${API.USER}/${API.INCOME}/${id}?page=${page}&size=${size}`,
     );
     return data;
@@ -52,7 +50,7 @@ export const loadIncome = async (historyData) => {
 // 수입내역 카테고리 별로 불러오기
 export const loadIncomeCategory = async (historyData) => {
     const { id, categoryId, page, size } = historyData;
-    const { data } = await apiFun.get(
+    const { data } = await authorizationClient.get(
         `${API.ACCOUNT}/${API.USER}/${API.INCOME}/${id}?page=${page}&size=${size}&categoryId=${categoryId}`,
     );
     return data;
@@ -61,7 +59,7 @@ export const loadIncomeCategory = async (historyData) => {
 // 해당 월 기록 불러오기
 export const loadMonthRecord = async (historyData) => {
     const { id, page, size, year, month } = historyData;
-    const { data } = await apiFun.get(
+    const { data } = await authorizationClient.get(
         `${API.ACCOUNT}/${API.USER}/${id}${API.ANALYZE}/month/${year}/${month}?page=${page}&size=${size}&${API.CATEGORY}`,
     );
     return data;
@@ -70,7 +68,7 @@ export const loadMonthRecord = async (historyData) => {
 // 일주일 기록 불러오기
 export const loadWeeklyCompareAnalyze = async (weeklyData) => {
     const { id, year, month, startDay } = weeklyData;
-    const { data } = await apiFun.get(
+    const { data } = await authorizationClient.get(
         `${API.ACCOUNT}/${id}${API.COMPARE_WEEKLY_ANALYZE}/${year}/${month}/${startDay}`,
     );
     return data;
--- pot_accounts/src/main/pot_front/src/api/main.js
+++ pot_accounts/src/main/pot_front/src/api/main.js
@@ -1,80 +1,41 @@
 import axios from "axios";
 import API from "../config/apiConfig";
-//import apiFun from './accounts';
 
-const storedMem = sessionStorage.getItem('mem');
-const mem =JSON.parse(storedMem);
-
-// 가계부 이름 불러오기
-export const accountInfo = async (id) => {
+// 맘껏써라 함수
+const fetchData = async (url, dataProcessor) => {
     try {
-        const response = await axios.get(`${API.ACCOUNT}/${id}`);
-        if (response.data) {
-            return response.data;
+        const res = await axios.get(url);
+        if (res.data) {
+            return dataProcessor(res.data);
         } else {
-            return "저장된 정보가 없습니다.";
+            console.log("데이터가 없습니다.");
+            return null;
         }
     } catch (error) {
-        console.error("가계부 정보 에러", error);
+        console.error("API 호출 에러", error);
         throw error;
     }
 };
 
+// 가계부 이름 불러오기
+export const accountInfo = async (id) => {
+    return fetchData(`${API.ACCOUNT}/${id}`, data => data);
+};
+
 // 월별 차트 데이터
-export const payMonth = async (id) =>{
-    try {
-        const res = await axios.get(`${API.ACCOUNT}/${id}/payMonth`);
-        if (res.data) {
-            return res.data;
-        } else {
-            return "저장된 지출정보가 없습니다.";
-        }
-    }catch (error){
-        console.log("지출내역에러" + error);
-        throw error;
-    }
-}
-export const incomeMonth = async (id) =>{
-    try {
-        const res = await axios.get(`${API.ACCOUNT}/${id}/incomeMonth`);
-        if (res.data) {
-            return res.data;
-        } else {
-            return "저장된 수입정보가 없습니다.";
-        }
-    }catch (error){
-        console.log("수입내역에러" , error);
-        throw error;
-    }
-}
+export const payMonth = async (id) => {
+    return fetchData(`${API.ACCOUNT}/${id}/payMonth`, data => data);
+};
+
+export const incomeMonth = async (id) => {
+    return fetchData(`${API.ACCOUNT}/${id}/incomeMonth`, data => data);
+};
 
 // 주별 차트 데이터
 export const payWeekly = async (id) => {
-        try {
-            const res = await axios.get(`${API.ACCOUNT}/${id}/payWeekly`);
-            if(res.data){
-                return res.data;
-            }else{
-                console.log("주별 지출 데이터 정보없음")
-            }
-        }catch (error){
-            console.log("지출 주별에러",error)
-            throw error;
-        }
-}
+    return fetchData(`${API.ACCOUNT}/${id}/payWeekly`, data => data);
+};
+
 export const incomeWeekly = async (id) => {
-    try{
-        const res = await axios.get(`${API.ACCOUNT}/${id}/incomeWeekly`)
-        if(res.data){
-            return res.data;
-        }else{
-            console.log("주별 수입 데이터 정보없음")
-        }
-    }catch (err){
-        console.log("수입 주별 에러",err)
-        throw err;
-    }
-}
-
-
-
+    return fetchData(`${API.ACCOUNT}/${id}/incomeWeekly`, data => data);
+};
