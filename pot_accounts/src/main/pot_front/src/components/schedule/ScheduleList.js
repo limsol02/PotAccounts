3896006ef0f18527d9da4986f5f0877ee0ba4b8c
YunheeAhn <ayh95944674@gmail.com>
2024-06-24 20:16:47 +0900

    메인페이지 일정 컴포넌트 구현

--- pot_accounts/src/main/pot_front/src/components/schedule/ScheduleList.js
+++ pot_accounts/src/main/pot_front/src/components/schedule/ScheduleList.js
@@ -3,6 +3,9 @@ import { useLocation, useNavigate } from "react-router-dom";
 import PATH from "../../pages/layout/data/path";
 import { AddScheduleBtn, DateTxt, InfoTxt, MemoTxt, NoneScheduleInner, NoneScheduleTxt, ScheduleBox, ScheduleLi, ScheduleUl } from "./ScheduleListStyle";
 import getDateUnit, { formatDate } from "../utils/date";
+import { useQuery } from "@tanstack/react-query";
+import QUERYKEYS from "../utils/querykey";
+import { loadSchedule, loadScheduleDetail } from "../../api/accounts";
 
 const ScheduleList = () => {
     const location = useLocation();
@@ -14,22 +17,36 @@ const ScheduleList = () => {
         navigate(PATH.CALENDAR)
     };
 
-    useEffect(() => {
-        const scheduleLists = [
-            { id: 1, date: '2024-06-01', memo: '회의', method: '지출', money: '10,000원' },
-            { id: 2, date: '2024-06-08', memo: '회의', method: '수입', money: '10,000원' },
-            { id: 3, date: '2024-06-27', memo: '회의', method: '지출', money: '10,000원' },
-            { id: 4, date: '2024-06-30', memo: '회의', method: '수입', money: '10,000원' },
-        ];
+    // 스케쥴 불러오는 상태관리
+    const useSchedule = () => {
+        return useQuery({
+            queryKey: [QUERYKEYS.LOAD_SCHEDULE],
+            queryFn: loadSchedule
+        });
+    };
+
+    const { data: scheduleData } = useSchedule();
+
+    useEffect(() => { 
+        if (!scheduleData) return ;
+
+        // const scheduleLists = [
+        //     { id: 1, date: '2024-06-01', memo: '회의', money: '10,000원' },
+        //     { id: 2, date: '2024-06-08', memo: '회의', money: '10,000원' },
+        //     { id: 3, date: '2024-06-27', memo: '회의', money: '10,000원' },
+        //     { id: 4, date: '2024-06-30', memo: '회의', money: '10,000원' },
+        // ];
 
         // 날짜 변환 함수 적용
-        const updatedSchedules = scheduleLists.map(schedule => ({
+        const updatedSchedules = scheduleData.map(schedule => ({
             ...schedule,
             date: getDateUnit(new Date(schedule.date))
         }));
-        setSchedules(updatedSchedules);
-    }, []);
 
+        setSchedules(updatedSchedules);
+    }, [scheduleData]);
+
+    // 남은 날짜별 색상변경
     const getScheduleColor = (date) => {
         const today = new Date();
         today.setHours(0, 0, 0, 0); // 시간 부분을 0으로 설정하여 날짜만 비교
@@ -37,7 +54,7 @@ const ScheduleList = () => {
         scheduleDate.setHours(0, 0, 0, 0); // 시간 부분을 0으로 설정
 
         const diffTime = scheduleDate - today;
-        const diffDays = Math.ceil((1000 * 60 * 60 * 24) / diffTime);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
         if (diffDays === 0) return '#0A3201'; // 당일
         if (diffDays <= 10) return '#20690B'; // 10일 이내
@@ -54,36 +71,34 @@ const ScheduleList = () => {
             <ul>
                 <li>
                     <MemoTxt>{schedule.memo}</MemoTxt>
-                    <InfoTxt>
-                        {schedule.method} / {schedule.money}
-                    </InfoTxt>
+                    <InfoTxt>{schedule.money}</InfoTxt>
                 </li>
             </ul>
         </ScheduleLi>
     );
 
-    return (
-        <div>
+        return (
             <div>
-                {schedules.length > 0 ? (
-                    <ScheduleBox>
-                        <ScheduleUl>
-                            {schedules.map((schedule) => (
-                                <ScheduleItem key={schedule.id} schedule={schedule} navigate={navigate} />
-                            ))}
-                        </ScheduleUl>
-                    </ScheduleBox>
-                ) : (
-                    <NoneScheduleInner>
-                        <NoneScheduleTxt>설정된 일정이 없습니다</NoneScheduleTxt>
-                        <AddScheduleBtn onClick={goToCalendar}>
-                            일정 추가하러 가기
-                        </AddScheduleBtn>
-                    </NoneScheduleInner>
-                )}
+                <div>
+                    {schedules.length > 0 ? (
+                        <ScheduleBox>
+                            <ScheduleUl>
+                                {schedules.map((schedule) => (
+                                    <ScheduleItem key={schedule.id} schedule={schedule} />
+                                ))}
+                            </ScheduleUl>
+                        </ScheduleBox>
+                    ) : (
+                        <NoneScheduleInner>
+                            <NoneScheduleTxt>설정된 일정이 없습니다</NoneScheduleTxt>
+                            <AddScheduleBtn onClick={goToCalendar}>
+                                일정 추가하러 가기
+                            </AddScheduleBtn>
+                        </NoneScheduleInner>
+                    )}
+                </div>
             </div>
-        </div>
-    );
+        );
 }
 
 export default ScheduleList;
