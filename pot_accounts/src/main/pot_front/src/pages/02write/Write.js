31cbd3997c8e91c5038413d17d034768dd80504d
YunheeAhn <ayh95944674@gmail.com>
2024-06-25 23:52:33 +0900

    입력 페이지 구성

--- pot_accounts/src/main/pot_front/src/pages/02write/Write.js
+++ pot_accounts/src/main/pot_front/src/pages/02write/Write.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Paginations from "./components/Pagination";
 
 const Write = () => {
     return (
         <div>
-            입력하기
+            <div> 
+                {/* 내역보기 드롭메뉴 */}
+                <div>
+                    <p>전체 내역보기</p>
+                    <p>|</p>
+                    <p>카테 고리내역보기</p>
+                </div>
+                {/* 내역 추가하기 */}
+                <button>
+                    + 내역 추가하기
+                </button>
+            </div>
+
+            <div>
+                <ul>
+                    <li>분류</li>
+                    <li>분류</li>
+                    <li>분류</li>
+                    <li>분류</li>
+                </ul>
+            </div>
+
+            <Paginations/>
         </div>
     )
 }
